ef737593de4548a1e889ca098d0cef1512e2176a
Pawankumar Jajara <pawankjajara@gmail.com>
2012-08-26 12:49:16 -0700

    Sample changes.

---
+++ src/main/java/Sample.java
@@ -0,0 +1,22 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pawankumar Jajara
+ * Date: 8/26/12
+ * Time: 12:47 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.lang.*;
+
+public class Sample {
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    int age;
+    String name;
+
+}
